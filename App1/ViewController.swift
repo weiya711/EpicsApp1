4af3d20a09c79cc6b8c500859ba64c259c3a4b04
olivia hsu <oliviahsu@airbears2-10-142-65-160.airbears2.1918.berkeley.edu>
2015-10-10 15:09:29 -0700

    facebook sdk login implementation

--- App1/ViewController.swift
+++ App1/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     //MARK: Properties
     @IBOutlet weak var nameTextField: UIStackView!
     @IBOutlet weak var usernameNameLabel: UIStackView!
@@ -16,11 +18,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if(FBSDKAccessToken.currentAccessToken() == nil){
+            print("Not Logged In")
+        }
+        else{
+            print("Logged In")
+        }
+        
+        var loginButton = FBSDKLoginButton()
+        loginButton.readPermissions = (["public_profile", "email", "user_friends"])
+        loginButton.center = self.view.center
+        
+        loginButton.delegate = self
+        
+        self.view.addSubview(loginButton)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //MARK: --Facebook Login
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!){
+        
+        if error == nil{
+            print("Login Complete")
+            self.performSegueWithIdentifier("showNew", sender: self)
+        }
+        else{
+            print(error.localizedDescription)
+        }
+        
+    }
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!){
+        print("User Logged Out")
     }
 
 
